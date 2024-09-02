17564987e9c8e8c90be314325e50a08fe037ba97
Been <63918877+BenGhad@users.noreply.github.com>
2024-09-02 23:53:13 +0000

    [LeetCode Sync] Runtime - 5 ms (18.45%), Memory - 55.3 MB (100.00%)

---
+++ my-folder/2132-convert-1d-array-into-2d-array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] ans = new int[m][n];
+        int ondex = 0;
+        if(original.length != m * n){
+            return new int[0][0];
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                ans[i][j] = original[ondex];
+                ondex++;
+            }
+        }
+        return ans;
+    }
+}
