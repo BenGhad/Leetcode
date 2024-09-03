74f85a3e35962ba64ca329d6f24a5b0d1039fe35
Been <63918877+BenGhad@users.noreply.github.com>
2024-09-03 13:14:16 +0000

    [LeetCode Sync] Runtime - 2 ms (75.30%), Memory - 41.2 MB (100.00%)

---
+++ my-folder/0274-h-index/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = citations.length;
+        for(int i = 0; i < citations.length; i++){
+            if(citations[i] >= h){
+                return h;
+            } else {
+                h--;
+            }
+        }
+        return 0;
+    }
+}
