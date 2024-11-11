25d49386c96197fccf46e02ed3a391fa7c6b3587
Been <63918877+BenGhad@users.noreply.github.com>
2024-11-11 16:31:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (99.99%)

--- my-folder/1146-greatest-common-divisor-of-strings/solution.java
+++ my-folder/1146-greatest-common-divisor-of-strings/solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if(!(str1 + str2).equals(str2 + str1)){
+        if(str1.length() < str2.length()){return gcdOfStrings(str2, str1);}
+        
+        if(!str1.startsWith(str2)){
             return "";
         }
-        return str1.substring(0, gcd(str1.length(), str2.length()));
-
-    }
-    public static int gcd(int a, int b){
-        if(b == 0){
-            return a;
+        if(str2.isEmpty()){
+            return str1;
         }
-        return gcd(b, a % b);
+        return(gcdOfStrings(str1.substring(str2.length()), str2));
     }
 }
