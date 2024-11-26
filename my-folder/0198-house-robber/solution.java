a7af9cb636d5890fc155323b0f15a59831560a4e
Been <63918877+BenGhad@users.noreply.github.com>
2024-11-26 04:49:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (99.99%)

--- my-folder/0198-house-robber/solution.java
+++ my-folder/0198-house-robber/solution.java
@@ -1,12 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        int rob = nums[0];
+        int n = nums.length;
+        int yes = nums[0];
         int no = 0;
+
         for(int i = 1; i < nums.length; i++){
-            int tmp = rob;
-            rob = Math.max(no + nums[i], rob);
-            no = tmp; 
+            int temp = no;
+            no = Math.max(no, yes);
+            yes = temp + nums[i];
         }
-        return Math.max(rob, no);
+        return Math.max(yes, no);
     }
 }
