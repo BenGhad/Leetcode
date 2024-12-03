f9af1e1b70ca865987125ac9b348334e93558481
Been <63918877+BenGhad@users.noreply.github.com>
2024-12-03 04:51:30 +0000

    [LeetCode Sync] Runtime - 2 ms (93.97%), Memory - 56.2 MB (99.93%)

--- my-folder/0238-product-of-array-except-self/solution.java
+++ my-folder/0238-product-of-array-except-self/solution.java
@@ -1,35 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        int[] l = new int[nums.length];
+        int[] r = new int[nums.length];
         int[] ans = new int[nums.length];
-        ans[0] = 1;
-        
+        l[0] = 1;
         for(int i = 1; i < nums.length; i++){
-            ans[i] = ans[i - 1] * nums[i - 1];
+            l[i] = l[i - 1] * nums[i - 1];
         }
-        int multi = 1;
-        for(int i = nums.length - 1; i >= 0; i--){
-            ans[i] *= multi;
-            multi *= nums[i];
+        r[nums.length - 1] = 1;
+        for(int i = nums.length - 2; i >= 0; i--){
+            r[i] = r[i + 1] * nums[i + 1];
         }
-
-/*
-        int[] pre = new int[nums.length];
-        int[] post  = new int[nums.length];
-    
-        pre[0] = 1;
-        for(int i = 1; i < nums.length; i++){ //o(n): populate pre
-            pre[i] = pre[i - 1] * nums[i - 1];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = r[i] * l[i];
         }
-        post[nums.length - 1] = 1;
-        for(int i = nums.length - 2; i >= 0; i--){ //o(n): populate post
-            post[i] = post[i + 1] * nums[i + 1];
-        }
-        for(int i = 0; i < nums.length; i++){ //o(n) ans
-            ans[i] = pre[i] * post[i];
-        }
-         */
-
-        
         return ans;
     }
 }
