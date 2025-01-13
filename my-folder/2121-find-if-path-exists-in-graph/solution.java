de2029d092d6c603c338a368cdf1c262ad1a0bcb
Been <63918877+BenGhad@users.noreply.github.com>
2025-01-13 19:46:07 +0000

    [LeetCode Sync] Runtime - 17 ms (93.41%), Memory - 123.6 MB (100.00%)

---
+++ my-folder/2121-find-if-path-exists-in-graph/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    int[] parent;
+    public boolean validPath(int n, int[][] edges, int source, int dest) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for (int[] edge : edges) {
+            union(edge[0], edge[1]);
+        }
+
+        return find(source) == find(dest);
+    }
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+}
