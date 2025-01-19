c801718f17a6dbf64438d678d376382944c75cd7
Been <63918877+BenGhad@users.noreply.github.com>
2025-01-19 21:56:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.5 MB (99.99%)

--- my-folder/0263-ugly-number/solution.java
+++ my-folder/0263-ugly-number/solution.java
@@ -1,38 +1,15 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n <= 0){
-            return false;
-        }
+        if(n <= 0) return false;
         while(n % 2 == 0){
-            n = n /2;
+            n /= 2;
         }
         while(n % 3 == 0){
-            n = n / 3;
+            n /= 3;
         }
         while(n % 5 == 0){
-            n = n / 5;
+            n /= 5;
         }
-
-        if(isPrime(n)){
-            return false;
-        }
-
-        for(int i = 7; i <= n / 2; i = i + 2){
-            if(n % i == 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public boolean isPrime(int n){
-        if(n == 1){ return false; }
-        for(int i = 5; i <= Math.sqrt(n); i = i + 6){
-            if(n % i == 0 || n % (i + 2) == 0){
-                return false;
-            }
-        }
-        return true;
+        return n == 1;
     }
 }
