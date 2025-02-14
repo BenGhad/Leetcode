9ecf3ca29e7aa67501965b8e40c28a15ce067a5c
Been <63918877+BenGhad@users.noreply.github.com>
2025-02-14 18:08:27 +0000

    [LeetCode Sync] Runtime - 31 ms (31.15%), Memory - 93.6 MB (100.00%)

--- my-folder/0380-insert-delete-getrandom-o1/solution.java
+++ my-folder/0380-insert-delete-getrandom-o1/solution.java
@@ -1,42 +1,49 @@
 class RandomizedSet {
-    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-    List<Integer> list = new ArrayList<Integer>();
+    /*
+    [1 4 2 5 8 9 ]
+     0 1 2 3 4 5 
+    
+    [1 4 9 5 8 ]
+     0 1 2 3 4
+        
 
+
+        
+        list.remove(2)
+
+        2 has index of 2
+        
+
+    */
+    List<Integer> set = new ArrayList<Integer>();
+    Map<Integer, Integer> map = new HashMap<>();
     public RandomizedSet() {
         
     }
     
     public boolean insert(int val) {
-        if(map.containsKey(val)){
-            return false;
-        } else {
-            list.add(val);
-            map.put(val, list.size() - 1);
-        }
+        if(map.containsKey(val)) return false;
 
+        set.add(val);
+        map.put(val, set.size() - 1);
         return true;
     }
     
     public boolean remove(int val) {
-        if(!map.containsKey(val)){
-            return false;
-        } else {
-            int last = list.getLast();
-            int index = map.get(val);
-
-            map.put(last, index);
-            list.set(index, last);
-            map.remove(val);
-            list.removeLast();
-        }
+        if(!map.containsKey(val)) return false;
+        int last = set.size() - 1;
+        int lastVal = set.get(last);
+        int idx = map.get(val);
+        set.set(idx, lastVal);
+        set.removeLast();
+        map.put(lastVal, idx);
+        map.remove(val);
         return true;
-        
     }
     
     public int getRandom() {
-        int random = (int)(Math.random() * list.size());
-
-        return list.get(random);
+        int random = (int)(Math.random() * set.size());
+        return set.get(random);
     }
 }
 
