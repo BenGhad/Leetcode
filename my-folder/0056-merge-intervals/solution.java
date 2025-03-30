39eca6420a29d835704e6d7f759d41a2ea7e3783
Been <63918877+BenGhad@users.noreply.github.com>
2025-03-30 23:28:06 +0000

    [LeetCode Sync] Runtime - 8 ms (92.12%), Memory - 46.6 MB (99.98%)

--- my-folder/0056-merge-intervals/solution.java
+++ my-folder/0056-merge-intervals/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length == 1) return intervals;
+        if (intervals.length == 1)
+            return intervals;
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> list = new ArrayList<int[]>();
-        for(int i = 0; i < intervals.length - 1; i++){
+        for (int i = 0; i < intervals.length - 1; i++) {
             int[] i1 = intervals[i];
             int[] i2 = intervals[i + 1];
-            if(i1[0] <= i2[0] && i2[0] <= i1[1]){
+            if (i1[0] <= i2[0] && i2[0] <= i1[1]) {
                 int[] merged = mergeInterval(i1, i2);
                 intervals[i + 1] = merged;
             } else {
@@ -14,17 +15,18 @@ class Solution {
             }
         }
         list.add(intervals[intervals.length - 1]);
-        
+
         int[][] result = new int[list.size()][2];
-        for(int i = 0; i < list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
         }
         return result;
     }
-    public int[] mergeInterval(int[] a, int[] b){
-        return new int[]{
-            Math.min(a[0], b[0]),
-            Math.max(a[1], b[1])
+
+    public int[] mergeInterval(int[] a, int[] b) {
+        return new int[] {
+                Math.min(a[0], b[0]),
+                Math.max(a[1], b[1])
         };
     }
 }
