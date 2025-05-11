51eb562b6e0b51d1492e78838aa00d7811296120
Been <63918877+BenGhad@users.noreply.github.com>
2025-05-11 22:31:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (99.95%)

---
+++ my-folder/1293-three-consecutive-odds/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        // base case:
+        if(arr.length < 3) return false;   
+        
+        int count = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 == 1){
+                count++;
+                if(count == 3) return true;
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
