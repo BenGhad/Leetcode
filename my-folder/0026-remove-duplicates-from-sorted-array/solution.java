e0bbeba39d0db252bb9069c075ea37664b769dfc
Been <63918877+BenGhad@users.noreply.github.com>
2025-06-23 16:31:19 +0000

    [LeetCode Sync] Runtime - 1 ms (78.59%), Memory - 45.1 MB (99.97%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.java
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0 || nums.length == 1){ return nums.length;}
         int k = 1;
-    
+        int ptr = 1;
         for(int i = 1; i < nums.length; i++){
-            if (nums[i] != nums[i-1]){
-                nums[k] = nums[i];
+            if(nums[i] != nums[i - 1]){
                 k++;
+                nums[ptr++] = nums[i];
             }
         }
+
         return k;
     }
 }
