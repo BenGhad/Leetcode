5d580b2fb35496d564dc6ff88c878bbb3580510e
Been <63918877+BenGhad@users.noreply.github.com>
2025-06-27 20:07:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (99.97%)

---
+++ my-folder/0747-min-cost-climbing-stairs/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int one = 0;
+        int two = 0;
+    
+        for(int i = 0; i < cost.length; i++){
+            int c = Math.min(one, two) + cost[i];    
+            two = one;
+            one = c;
+        }
+        
+        return Math.min(two, one);
+        
+        
+    }
+}
