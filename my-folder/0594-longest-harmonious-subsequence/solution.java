267fe56dafb9559a9ce4228726dea25ece57e185
Been <63918877+BenGhad@users.noreply.github.com>
2025-06-30 03:45:35 +0000

    [LeetCode Sync] Runtime - 28 ms (7.60%), Memory - 46.2 MB (100.00%)

---
+++ my-folder/0594-longest-harmonious-subsequence/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int max = 0;
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        
+            if(map.containsKey(num + 1)){
+                max = Math.max(max, map.get(num) + map.get(num + 1));
+            }
+            if(map.containsKey(num - 1)){
+                max = Math.max(max, map.get(num) + map.get(num - 1));
+            }
+        }
+
+        return max;
+    }
+}
