e06bae2b0bcdcfc029d7dffbf2be69a3a7de4845
Been <63918877+BenGhad@users.noreply.github.com>
2025-07-11 02:32:53 +0000

    [LeetCode Sync] Runtime - 7 ms (100.00%), Memory - 45.4 MB (99.97%)

---
+++ my-folder/1708-design-parking-system/solution.java
@@ -0,0 +1,18 @@
+class ParkingSystem {
+    int[] lot;
+    public ParkingSystem(int big, int medium, int small) {
+        lot = new int[]{
+            big, medium, small
+        };
+    }
+    
+    public boolean addCar(int carType) {
+        return lot[carType - 1]-- > 0;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
