66ab5c91c71a7721631d3456ab6ddb718455fbf6
Been <63918877+BenGhad@users.noreply.github.com>
2025-07-14 00:59:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.8 MB (99.99%)

--- my-folder/0191-number-of-1-bits/solution.java
+++ my-folder/0191-number-of-1-bits/solution.java
@@ -1,15 +1,5 @@
 class Solution {
     public int hammingWeight(int n) {
-        /* idea:
-        if odd: weight++
-        divide by 2
-        */
-        int weight = 0;
-        while(n > 0){
-            if(n % 2 == 1) weight++;
-            n /= 2;
-        }
-        
-        return weight;
+        return Integer.bitCount(n);
     }
 }
