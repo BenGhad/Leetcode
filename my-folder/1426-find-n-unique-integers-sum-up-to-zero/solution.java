5ecd5abc09258edf2ff02d72ddf3f96dd5ab3e83
Been <63918877+BenGhad@users.noreply.github.com>
2025-09-07 03:50:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (99.88%)

---
+++ my-folder/1426-find-n-unique-integers-sum-up-to-zero/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        for(int i = 0; i < 2 * (n / 2); i+= 2){
+            ans[i] = i + 1;
+            ans[i + 1] = 0 - (i + 1);
+        }   
+        if(n % 2 != 0){
+            ans[n - 1] = 0;
+        }
+
+        return ans;
+    }
+}
