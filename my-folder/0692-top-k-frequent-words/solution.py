de6d3fa2508d6a18daa3af410ee54f14e27eb076
Been <63918877+BenGhad@users.noreply.github.com>
2025-11-19 21:06:14 +0000

    [LeetCode Sync] Runtime - 1 ms (68.35%), Memory - 17.7 MB (100.00%)

---
+++ my-folder/0692-top-k-frequent-words/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = Counter(words)
+        items = sorted(freq.items(), key=lambda x: (-x[1], x[0]))
+        return [w for w, _ in items[:k]]
