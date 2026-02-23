a93aa0759ecd61c113a37c40ce269383ea4a9804
Been <63918877+BenGhad@users.noreply.github.com>
2026-02-23 01:23:50 +0000

    [LeetCode Sync] Runtime - 5 ms (0.71%), Memory - 43.7 MB (8.86%)

--- my-folder/0066-plus-one/solution.java
+++ my-folder/0066-plus-one/solution.java
@@ -1,17 +1,24 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int[] d = new int[digits.length + 1];
-        System.arraycopy(digits, 0, d, 1, digits.length);        
-        for(int i = d.length - 1; i > 0; i--){
-            if(d[i] != 9){
-                d[i]++;
-                return Arrays.copyOfRange(d, 1, d.length);
-            } else {
-                d[i] = 0;
-            }
-        }
-        d[0] = 1;
-        return d;
+        Stack<Integer> s = new Stack<>();
 
+        boolean c = true;
+        for(int i = digits.length - 1; i >= 0; i--){
+            int d = digits[i];
+            if(c){
+                d += 1;
+            }
+            c = d==10;
+            d %= 10;
+            s.push(d);
+        }
+
+        ArrayList<Integer> num = new ArrayList<>();
+        if(c) {num.add(1);}
+        while(!s.isEmpty()){
+            num.add(s.pop());
+        }
+
+        return num.stream().mapToInt(i -> i).toArray();
     }
 }
