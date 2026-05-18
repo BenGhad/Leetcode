dc39ca3aa3a9fb4d5fb624ded80524ff71710c0f
Been <63918877+BenGhad@users.noreply.github.com>
2026-05-18 22:41:58 +0000

    [LeetCode Sync] Runtime - 267 ms (62.51%), Memory - 68.8 MB (67.84%)

---
+++ my-folder/0595-big-countries/solution.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    # return world.query('area >= 3000000 or population >= 25000000').filter(items=['name','population','area'])
+    return world.loc[
+        (world["area"] >= 3000000) | (world["population"] >= 25000000),
+        ["name", "population", "area"]
+    ]
