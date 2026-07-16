04f8bd59b75d70156c90d38dd46888e5651f67d2
Been <63918877+BenGhad@users.noreply.github.com>
2026-07-16 19:18:14 +0000

    [LeetCode Sync] Runtime - 47 ms (98.27%), Memory - 27.9 MB (59.73%)

---
+++ my-folder/0931-maximum-frequency-stack/solution.py
@@ -0,0 +1,26 @@
+class FreqStack:
+    
+    def __init__(self):
+        self.stacks = []
+        self.freq = defaultdict(int)
+
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        if self.freq[val] > len(self.stacks):
+            self.stacks.append([val])
+        else:
+            self.stacks[self.freq[val] - 1].append(val)
+    
+    def pop(self) -> int:
+        val = self.stacks[-1].pop()
+        if not self.stacks[-1]:
+            self.stacks.pop()
+        self.freq[val]-=1
+        return val    
+
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
