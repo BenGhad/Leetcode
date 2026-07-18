5bd552d08c1a6ecbdd2df84f72ebeb0eb4a48106
Been <63918877+BenGhad@users.noreply.github.com>
2026-07-18 22:30:21 +0000

    [LeetCode Sync] Runtime - 9 ms (5.06%), Memory - 46.3 MB (6.45%)

--- my-folder/0009-palindrome-number/solution.java
+++ my-folder/0009-palindrome-number/solution.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) {
-            return false;
-            }
-        if(x < 10) { 
-            return true;
-            }
-        if(x < 100) {
-            return (x % 10) == (x/10);
-            }
-
-        String str = Integer.toString(x);
-        int len = str.length();
-        String one = str.substring(0, (len / 2));
-        StringBuilder two = new StringBuilder();
-
-        int startIdx = len % 2 == 0 ? len / 2 : len / 2 + 1; 
-        for(int i = len - 1; i >= startIdx; i--){
-            two.append(str.charAt(i));
+        if(x == 0) return true;
+        if(x < 0) return false;
+        
+        int numDigs = (int)Math.log10(x) + 1;
+        
+        Stack<Integer> digs = new Stack<>();
+        
+        for(int i = 0; i < numDigs / 2; i++){
+            digs.push(x % 10);
+            x /= 10;
         }
-
-        return one.equals(two.toString());
+        
+        if(numDigs % 2 != 0) x /= 10;
+        
+        while(!digs.isEmpty()){
+            if(x % 10 != digs.pop()) return false;
+            x /= 10;
+        }
+        return true;
     }
 }
-
