2959365bf2fbadf2d9c54951781d92784fabb7c0
Been <63918877+BenGhad@users.noreply.github.com>
2026-07-18 22:22:33 +0000

    [LeetCode Sync] Runtime - 1 ms (94.27%), Memory - 43.6 MB (33.58%)

--- my-folder/0118-pascals-triangle/solution.java
+++ my-folder/0118-pascals-triangle/solution.java
@@ -1,27 +1,24 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        l1.add(1);
-        l2.add(1);
-        l2.add(1);
-        list.add(l1);
-        if(numRows == 1) return list;
-        list.add(l2);
-        if(numRows == 2) return list;
-
-        for(int i = 3; i <= numRows; i++){
-            List<Integer> li = new ArrayList<>();
-            li.add(1);
-            for(int j = 0; j < i - 2; j++){
-                int v = list.get(i - 2).get(j) + list.get(i - 2).get(j + 1); 
-                li.add(v);
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for(int i = 0; i < numRows; i++) ans.add(new ArrayList<>());
+        
+        //numrows >=1 
+        ans.get(0).add(1);
+        
+        for(int j = 1; j < numRows; j++){
+            List<Integer> prv = ans.get(j-1);
+            List<Integer> cur = ans.get(j);
+            
+            cur.add(1);
+            
+            for(int i = 1; i < prv.size(); i++){
+                cur.add(prv.get(i-1) + prv.get(i));
             }
-            li.add(1);
-            list.add(li);
+            
+            cur.add(1);
         }
-
-        return list;
+        return ans;
     }
 }
