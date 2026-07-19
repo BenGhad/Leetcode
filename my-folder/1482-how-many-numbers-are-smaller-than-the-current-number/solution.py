09845530e0476eef57aa2b047542ce8a65975ac1
Been <63918877+BenGhad@users.noreply.github.com>
2026-07-19 20:57:02 +0000

    [LeetCode Sync] Runtime - 155 ms (16.73%), Memory - 19.3 MB (21.75%)

--- my-folder/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
+++ my-folder/1482-how-many-numbers-are-smaller-than-the-current-number/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0] * n
-        for i in range(n):
-            cout = 0
-            for j in range(n):
-                if i == j: continue
-                if nums[j] < nums[i]: cout += 1
-            ans[i] = cout
+        ans = []
+        for num in nums:
+            ans.append(0)
+            for n2 in nums:
+                if n2 < num:
+                    ans[-1] += 1
         return ans
+                
