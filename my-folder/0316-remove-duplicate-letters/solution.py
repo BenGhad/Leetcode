226229249caa2fde63679f7c01af4c2ca75a13ab
Been <63918877+BenGhad@users.noreply.github.com>
2026-07-19 04:41:53 +0000

    [LeetCode Sync] Runtime - 3 ms (62.74%), Memory - 19.4 MB (45.48%)

---
+++ my-folder/0316-remove-duplicate-letters/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last = {}
+        n = len(s)
+        for i in range(n):
+            last[s[i]] = i
+
+        stack = []
+        used = set()
+
+        for i in range(n):
+            c = s[i]
+            if c in used:
+                continue
+                
+
+            while stack and stack[-1] > c and last[stack[-1]] > i:
+                used.remove(stack.pop())
+
+            stack.append(c)
+            used.add(c)
+
+        return "".join(stack)
